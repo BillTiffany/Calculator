6d4a074a8efcc988deb6a1bb0689f76f12de052c
Bill Tiffany <daganlegalservice@gmail.com>
2021-09-20 14:17:46 -0600

    Initial commit

--- index.js
+++ index.js
@@ -1,1 +1,45 @@
-console.log("hello world");
+const readline = require('readline')
+
+const reader = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+  })
+  
+  reader.question("Howdy, what's your name?", function(answer) {
+    console.log("Hi " + answer);
+  });
+  
+
+
+const name = "Bill"
+
+let age = 39
+
+console.log("this is the name variable,", name);
+console.log("this is the age variable", age);
+
+const isCool = true; 
+let car = null;
+
+let height; 
+
+let x = 4;
+
+let y = 10;
+
+let z = 7+8;
+
+let sum = x+y;
+
+console.log(x+y+z);
+console.log(height);
+console.log(sum);
+
+let str = "This is a string,\r\n";
+let strTwo = " this is a second string, \r\n "
+let strThree = ` this is my name: ${name}, \r\n 
+and this is my age ${age}` ;
+
+let strings = str + strTwo + strThree 
+
+console.log(strings)
